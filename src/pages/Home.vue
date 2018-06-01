a3dd5d43c1f12fd4bbaec08e29f36b6a6f331f14
Pierre Angot <pierre.angot@verteego.com>
2018-06-01 21:50:41 +0200

    update range

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-      <Range ref="range"/>
+    <v-flex xs12 class="text-xs-center">
+      <h1>Vous êtes le Maître du Jeu</h1>
+      <h3>Combien de personnes jouent avec vous ?</h3>
+    </v-flex>
+    <v-flex xs8 offset-xs2>
+      <v-slider
+        v-model="value"
+        thumb-label
+        step="1"
+        ticks
+        :min="4"
+        :max="10"
+        ></v-slider>
+    </v-flex>
+    <v-flex xs4 offset-xs4 center>
+      <h2 class="text-xs-center">{{ value }}</h2>
+    </v-flex>
+    <v-flex xs4 offset-xs4 center>
+      <v-btn block color="primary" dark>Play</v-btn>
+    </v-flex>
   </div>
 </template>
 
@@ -11,6 +30,11 @@ export default {
   name: "Home",
   components: {
     Range
+  },
+  data () {
+    return {
+      value: 0
+    }
   }
 };
 </script>
