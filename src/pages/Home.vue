b28c5bf00275d8a933127f8df719622d7e37b04f
Felix Becquart <felixbecquart1990@gmail.com>
2018-06-01 21:21:42 +0200

    architecture

---
+++ src/pages/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <Range ref="range"/>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "Home",
+  components: {
+    Range: () => import("@/components/Range"),
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
